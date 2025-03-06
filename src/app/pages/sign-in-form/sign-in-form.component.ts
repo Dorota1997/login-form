d3da02a2d05c195e4afbcbf16a35db1f90a19eef
Dorota1997 <gildota@wp.pl>
2025-03-06 19:46:07 +0100

    Generate token after submit form

--- src/app/pages/sign-in-form/sign-in-form.component.ts
+++ src/app/pages/sign-in-form/sign-in-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, inject } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { emailPatternValidator } from '../../validators/email.validator';
 import {
   FormBuilder,
   FormGroup,
@@ -15,9 +17,16 @@ import {
 })
 export class SignInFormComponent {
   private formBuilder = inject(FormBuilder);
+  private authService = inject(AuthService);
 
   signInForm: FormGroup = this.formBuilder.group({
     email: ['', [Validators.required, emailPatternValidator()]],
     password: ['', Validators.required],
   });
+
+  onSubmit(): void {
+    if (this.signInForm.valid) {
+      this.authService.generateToken();
+    }
+  }
 }
