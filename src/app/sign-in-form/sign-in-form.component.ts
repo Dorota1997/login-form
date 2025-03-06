8265760da0035adf74a25162539a31e515e76ec7
Dorota1997 <gildota@wp.pl>
2025-03-06 14:23:19 +0100

    Create sign in form component

---
+++ src/app/sign-in-form/sign-in-form.component.ts
@@ -0,0 +1,22 @@
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-sign-in-form',
+  imports: [ReactiveFormsModule],
+  templateUrl: './sign-in-form.component.html',
+  styleUrl: './sign-in-form.component.scss',
+})
+export class SignInFormComponent {
+  private formBuilder = inject(FormBuilder);
+
+  signInForm: FormGroup = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+}
